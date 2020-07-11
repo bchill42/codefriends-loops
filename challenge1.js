a53f004b05a0108c36c88437f41cb339547bb2e7
Brandon Hill <brandon@climbalytics.com>
2020-07-11 18:51:12 -0500

    added comment explaing what the challenge is

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,7 @@
+// Challenge #1
+// If we list all the whole numbers between 1 and 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
 let sum = 0;
 for (let i = 0; i < 1000; i++) {
     if ((i % 3 === 0) || (i % 5 === 0)) {
